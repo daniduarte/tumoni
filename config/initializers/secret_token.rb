0bee822bdd85c5870823c29b852f18644071ca27
Daniel Duarte <elduarte@gmail.com>
2013-12-14 14:07:57 -0300

    Primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tumoni2::Application.config.secret_key_base = 'ef664f5a968c297d7aef317fb85b68a09e61fcbcae06796b07ec03033218e25f31694eb171ae1d498597bc4b00b75550e37c3b980e188f929ae2daffc934e98d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tumoni2::Application.config.session_store :cookie_store, key: '_tumoni2_session'
