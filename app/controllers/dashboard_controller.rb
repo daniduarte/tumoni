5db2faa38cc645dc4e28a41686bf42c1484a6b60
Daniel Duarte <elduarte@gmail.com>
2014-02-10 14:37:17 -0300

    Validacion de usuarios en historial

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,10 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!
   
   def index
-    @movement_history = MovementHistory.all
+    if current_user.type_id == 1
+      @movement_history = MovementHistory.all
+    else
+      redirect_to pockets_path
+    end
   end
 end
